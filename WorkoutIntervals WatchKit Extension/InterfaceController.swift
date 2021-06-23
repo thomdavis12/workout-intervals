577e48183afd782dab787d65f958f6465f33acf2
thomasdavis <thomas.davis@terminus.com>
2021-06-23 13:24:45 -0400

    ADDED onOffSwitch and onOffText

--- WorkoutIntervals WatchKit Extension/InterfaceController.swift
+++ WorkoutIntervals WatchKit Extension/InterfaceController.swift
@@ -10,17 +10,29 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
+    @IBOutlet weak var onOffSwitch: WKInterfaceSwitch!
+    @IBOutlet var onOffText: WKInterfaceTextField!
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        onOffText.setText("On");
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
-
+    
+    @IBAction func setText(_ value: Bool) {
+        if (value) {
+            onOffText.setText("On");
+        }
+        else {
+            onOffText.setText("Off");
+        }
+    }
 }
